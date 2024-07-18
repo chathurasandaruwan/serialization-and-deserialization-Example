56ce1422ba099ce908099f53fe3feaa9b2fcc9e6
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-07-18 08:45:28 +0530

    create Student class

--- src/main/java/lk/ijse/Student.java
+++ src/main/java/lk/ijse/Student.java
@@ -1,4 +1,15 @@
 package lk.ijse;
 
-public class Student {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Student implements Serializable {
+    private String id;
+    private String name;
+    private String address;
 }
