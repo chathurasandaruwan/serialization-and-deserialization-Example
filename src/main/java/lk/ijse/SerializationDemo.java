9b4f6a6fc07a64ccd85d8a96b6283962e5f04a2b
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-07-18 10:19:00 +0530

    create Serialization part

--- src/main/java/lk/ijse/SerializationDemo.java
+++ src/main/java/lk/ijse/SerializationDemo.java
@@ -1,4 +1,20 @@
 package lk.ijse;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class SerializationDemo {
+    public static void main(String[] args) {
+        Student student = new Student("1","chathura","mathugama");
+
+            try {
+                FileOutputStream file = new FileOutputStream("student.ser");
+                ObjectOutputStream out = new ObjectOutputStream(file);
+                out.writeObject(student);
+                System.out.println("Object has been Serialized");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+    }
 }
